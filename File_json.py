b78c3d79461d71918caae4bee64958a6b22621df
hieutran208 <hieuhust2001@gmail.com>
2026-03-12 14:08:51 +0700

    Change File_json.py

--- File_json.py
+++ File_json.py
@@ -1,14 +1,28 @@
 import json
 
-data = {
-    "id": "001",
-    "name": "Hiếu",
-    "age": 25,
-    "skills": ["Python", "SQL"]
+dict_data = {
+ "user":[
+    {"id":1,"name":"An"},
+    {"id":2,"name":"Binh"}
+ ],
+ "skills":["Python","SQL"],
+ "address":{
+    "city":"Hanoi",
+    "country":"Vietnam"
+ }
 }
-
+# Ghi file .json
 with open('exp.json',mode='w',encoding='utf-8') as file:
-    json.dump(data, file, indent=4, ensure_ascii=False) # indent = 4: Xuống dòng và thụt vào 4 dấu cách (nếu không có thì kqua sẽ dưới dạng 1 dòng)
-    json.dumps(data, indent=4, ensure_ascii=False) # json.dump là ghi vào file, còn json.dumps là chuyển thành chuỗi để in ra trực tiếp
-    print(json.dumps(data, indent=4, ensure_ascii=False)) # ensure_ascii=False → giữ nguyên ký tự Unicode (thường dùng khi có tiếng Việt)
-    # ensure_ascii=True => File JSON chỉ chứa ký tự ASCII. Tất cả kí tự Unicode sẽ bị chuyển thành dạng \uXXXX.
+    json.dump(dict_data, file, indent=4, ensure_ascii=False) 
+    json.dumps(dict_data, indent=4, ensure_ascii=False)
+    print(json.dumps(dict_data, indent=4, ensure_ascii=False))
+# Đọc file .json
+with open('exp.json',mode='r',encoding='utf-8') as file: # encoding="utf-8" để tránh lỗi Unicode
+    data = json.load(file)
+    print(data)
+    print(type(data)) 
+    print(data["skills"]) # có thể dùng data.get("name") để truy cập dữ liệu
+    print(data["address"]["city"]) # Đọc JSON lồng nhau
+    print(data["skills"][0]) # Data có list bên trong Object
+    for user in data["user"]: # data["user"] là một list => Truy cập vào các phần tử trong list
+        print(f"ID: {user["id"]} Tên: {user["name"]}")
